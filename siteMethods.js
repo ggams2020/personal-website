7f49a0a60756521158ceea4a11fd062c7d73d370
Genna Gams <ggams@berkeley.edu>
2022-09-13 23:28:14 -0700

    more testing

--- siteMethods.js
+++ siteMethods.js
@@ -1,6 +1,7 @@
 
 $().ready(function() {
     // Function to load the navbar to pages on the site. 
+    console.log("got here");
     $("#navbar").load("templates/navbar.html");
     console.log("success");
 });
