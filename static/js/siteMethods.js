d6889e8f4d981472d8205eb0ed92474827594c1a
Genna Gams <ggams@berkeley.edu>
2022-09-13 23:50:33 -0700

    removed js calls

--- static/js/siteMethods.js
+++
@@ -1,7 +0,0 @@
-
-$().ready(function() {
-    // Function to load the navbar to pages on the site. 
-    console.log("got here");
-    $("#navbar").load("templates/navbar.html");
-    console.log("success");
-});
